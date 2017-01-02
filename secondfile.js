7bb2cfef24f6b679101e9c98a8d3d2754bee0c4a
JunSeokJeong <26wnstjr@naver.com>
2017-01-02 12:49:44 +0900

    yes

--- secondfile.js
+++ secondfile.js
@@ -1,3 +1,5 @@
 console.log('Hello, my name is JunSeokJeong');
 console.log('Welcome to the my gitFolder');
 console.log('We are do to the manner play game');
+consloe.log('where is my turn??');
+console.log('It is that goods email');
