a1b011e394a6402ffc50ba2b8bcf85b74181f722
JunSeokJeong <26wnstjr@naver.com>
2017-01-05 10:31:29 +0900

    dfads

--- secondfile.js
+++ secondfile.js
@@ -4,3 +4,5 @@ console.log('We are do to the manner play game');
 consloe.log('where is my turn??');
 console.log('It is that goods email');
 console.log('Are you crazy??');
+console.log('Teme~~');
+console.log('nanisurunda?');
